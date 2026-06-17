ab3cccfe1bb00832b77a1701955e482398fc793c
Juliana Santos <juliana_jns_2010@hotmail.com>
2026-06-17 04:38:07 -0300

    refactor: substitui options fixas por arrays de cargos e estados - criado array de cargos e de estados - feito select com foreach

--- app/Views/funcionarios/index.php
+++ app/Views/funcionarios/index.php
@@ -15,6 +15,65 @@ $cpfValue = $isEdit ? $funcionario->getCpf() : ($cpfBusca ?? '');
 $titulo = 'Funcionários';
 $favicon = '/ideal/public/assets/icon/funcionario2.png';
 
+
+
+//  ARRAY PARA OS CARGOS 
+$cargos = [
+    'Almoxarife',
+    'Analista Financeiro',
+    'Assistente Administrativo',
+    'Assistente de RH',
+    'Auxiliar Administrativo',
+    'Auxiliar de Eletricista',
+    'Cabista',
+    'Comprador',
+    'Designer Gráfico',
+    'Eletricista de Manutenção',
+    'Eletricista Industrial',
+    'Eletricista Montador',
+    'Eletricista Predial',
+    'Encarregado de Obras Elétricas',
+    'Instalador Elétrico',
+    'Mestre de Obras',
+    'Montador de Painéis Elétricos',
+    'Oficial Eletricista'
+];
+
+sort($cargos);
+//  ARRAY PARA OS ESTADOS
+
+$estados = [
+    'AC' => 'Acre',
+    'AL' => 'Alagoas',
+    'AP' => 'Amapá',
+    'AM' => 'Amazonas',
+    'BA' => 'Bahia',
+    'CE' => 'Ceará',
+    'DF' => 'Distrito Federal',
+    'ES' => 'Espírito Santo',
+    'GO' => 'Goiás',
+    'MA' => 'Maranhão',
+    'MT' => 'Mato Grosso',
+    'MS' => 'Mato Grosso do Sul',
+    'MG' => 'Minas Gerais',
+    'PA' => 'Pará',
+    'PB' => 'Paraíba',
+    'PR' => 'Paraná',
+    'PE' => 'Pernambuco',
+    'PI' => 'Piauí',
+    'RJ' => 'Rio de Janeiro',
+    'RN' => 'Rio Grande do Norte',
+    'RS' => 'Rio Grande do Sul',
+    'RO' => 'Rondônia',
+    'RR' => 'Roraima',
+    'SC' => 'Santa Catarina',
+    'SP' => 'São Paulo',
+    'SE' => 'Sergipe',
+    'TO' => 'Tocantins'
+];
+
+
+
 //HEADER
 require_once __DIR__ . '/../includes/header.php';
 ?>
@@ -38,7 +97,7 @@ require_once __DIR__ . '/../includes/header.php';
                 <div class="grid-busca">
                     <div class="busca-box">
                         <h2>
-                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <!-- <i class="fa-solid fa-magnifying-glass"></i> -->
                             BUSCAR FUNCIONÁRIO
                         </h2>
 
@@ -54,7 +113,7 @@ require_once __DIR__ . '/../includes/header.php';
                                 <input type="text" name="cpf" oninput="mascaraCPF(this)" placeholder="000.000.000-00"
                                     required maxlength="14">
                             </div>
-                            <button type="submit" class="btn-buscar"><i class="bi bi-search"></i>  BUSCAR</button>
+                            <button type="submit" class="btn-buscar"><i class="bi bi-search"></i> BUSCAR</button>
                         </form>
                     </div>
 
@@ -86,7 +145,8 @@ require_once __DIR__ . '/../includes/header.php';
                     <?php unset($_SESSION['mensagem_erro']); ?>
                 <?php endif; ?>
 
-                <h2>Dados do Funcionário</h2>
+
+                <h2> <i class="fa-regular fa-clipboard icone-titulo"></i> Dados do Funcionário</h2>
 
                 <form id="form-dados" action="<?= $actionUrl ?>" method="POST">
                     <div class="grid-form">
@@ -124,35 +184,13 @@ require_once __DIR__ . '/../includes/header.php';
 
                         <div class="form-group">
                             <label>Estado Nasc.</label>
-                            <select name="estadoNascimento" required>
-                                <option value="">Selecione o Estado</option>
-                                <option value="AC" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'AC' ? 'selected' : '' ?>>Acre</option>
-                                <option value="AL" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'AL' ? 'selected' : '' ?>>Alagoas</option>
-                                <option value="AP" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'AP' ? 'selected' : '' ?>>Amapá</option>
-                                <option value="AM" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'AM' ? 'selected' : '' ?>>Amazonas</option>
-                                <option value="BA" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'BA' ? 'selected' : '' ?>>Bahia</option>
-                                <option value="CE" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'CE' ? 'selected' : '' ?>>Ceará</option>
-                                <option value="DF" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'DF' ? 'selected' : '' ?>>Distrito Federal</option>
-                                <option value="ES" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'ES' ? 'selected' : '' ?>>Espírito Santo</option>
-                                <option value="GO" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'GO' ? 'selected' : '' ?>>Goiás</option>
-                                <option value="MA" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'MA' ? 'selected' : '' ?>>Maranhão</option>
-                                <option value="MT" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'MT' ? 'selected' : '' ?>>Mato Grosso</option>
-                                <option value="MS" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'MS' ? 'selected' : '' ?>>Mato Grosso do Sul</option>
-                                <option value="MG" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'MG' ? 'selected' : '' ?>>Minas Gerais</option>
-                                <option value="PA" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'PA' ? 'selected' : '' ?>>Pará</option>
-                                <option value="PB" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'PB' ? 'selected' : '' ?>>Paraíba</option>
-                                <option value="PR" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'PR' ? 'selected' : '' ?>>Paraná</option>
-                                <option value="PE" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'PE' ? 'selected' : '' ?>>Pernambuco</option>
-                                <option value="PI" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'PI' ? 'selected' : '' ?>>Piauí</option>
-                                <option value="RJ" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'RJ' ? 'selected' : '' ?>>Rio de Janeiro</option>
-                                <option value="RN" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'RN' ? 'selected' : '' ?>>Rio Grande do Norte</option>
-                                <option value="RS" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'RS' ? 'selected' : '' ?>>Rio Grande do Sul</option>
-                                <option value="RO" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'RO' ? 'selected' : '' ?>>Rondônia</option>
-                                <option value="RR" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'RR' ? 'selected' : '' ?>>Roraima</option>
-                                <option value="SC" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'SC' ? 'selected' : '' ?>>Santa Catarina</option>
-                                <option value="SP" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'SP' ? 'selected' : '' ?>>São Paulo</option>
-                                <option value="SE" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'SE' ? 'selected' : '' ?>>Sergipe</option>
-                                <option value="TO" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === 'TO' ? 'selected' : '' ?>>Tocantins</option>
+                            <select name="estadoNascimento">
+                                <option value="">Selecione</option>
+                                <?php foreach ($estados as $sigla => $nome): ?>
+                                    <option value="<?= $sigla ?>" <?= ($isEdit ? $funcionario->getEstadoNascimento() : '') === $sigla ? 'selected' : '' ?>>
+                                        <?= $nome ?>
+                                    </option>
+                                <?php endforeach; ?>
                             </select>
                         </div>
 
@@ -164,17 +202,16 @@ require_once __DIR__ . '/../includes/header.php';
 
                         <div class="form-group">
                             <label>Cargo / Função</label>
+
                             <select name="cargoFuncao">
                                 <option value="">Selecione</option>
-                                <option value="Auxiliar Administrativo" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === 'Auxiliar Administrativo' ? 'selected' : '' ?>>Auxiliar Administrativo
-                                </option>
-                                <option value="Auxiliar de RH" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === 'Auxiliar de RH' ? 'selected' : '' ?>>Auxiliar de RH</option>
-                                <option value="Azulejista" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === 'Azulejista' ? 'selected' : '' ?>>Azulejista</option>
-                                <option value="Eletricista" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === 'Eletricista' ? 'selected' : '' ?>>Eletricista</option>
-                                <option value="Encarregado" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === 'Encarregado' ? 'selected' : '' ?>>Encarregado</option>
-                                <option value="Marceneiro" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === 'Marceneiro' ? 'selected' : '' ?>>Marceneiro</option>
-                                <option value="Pintor" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === 'Pintor' ? 'selected' : '' ?>>Pintor</option>
+                                <?php foreach ($cargos as $cargo): ?>
+                                    <option value="<?= $cargo ?>" <?= ($isEdit ? $funcionario->getCargoFuncao() : '') === $cargo ? 'selected' : '' ?>>
+                                        <?= $cargo ?>
+                                    </option>
+                                <?php endforeach; ?>
                             </select>
+
                         </div>
 
                         <div class="form-group">
@@ -216,35 +253,13 @@ require_once __DIR__ . '/../includes/header.php';
 
                         <div class="form-group">
                             <label>Estado</label>
-                            <select name="estado" required>
-                                <option value="">Selecione o Estado</option>
-                                <option value="AC" <?= ($isEdit ? $funcionario->getEstado() : '') === 'AC' ? 'selected' : '' ?>>Acre</option>
-                                <option value="AL" <?= ($isEdit ? $funcionario->getEstado() : '') === 'AL' ? 'selected' : '' ?>>Alagoas</option>
-                                <option value="AP" <?= ($isEdit ? $funcionario->getEstado() : '') === 'AP' ? 'selected' : '' ?>>Amapá</option>
-                                <option value="AM" <?= ($isEdit ? $funcionario->getEstado() : '') === 'AM' ? 'selected' : '' ?>>Amazonas</option>
-                                <option value="BA" <?= ($isEdit ? $funcionario->getEstado() : '') === 'BA' ? 'selected' : '' ?>>Bahia</option>
-                                <option value="CE" <?= ($isEdit ? $funcionario->getEstado() : '') === 'CE' ? 'selected' : '' ?>>Ceará</option>
-                                <option value="DF" <?= ($isEdit ? $funcionario->getEstado() : '') === 'DF' ? 'selected' : '' ?>>Distrito Federal</option>
-                                <option value="ES" <?= ($isEdit ? $funcionario->getEstado() : '') === 'ES' ? 'selected' : '' ?>>Espírito Santo</option>
-                                <option value="GO" <?= ($isEdit ? $funcionario->getEstado() : '') === 'GO' ? 'selected' : '' ?>>Goiás</option>
-                                <option value="MA" <?= ($isEdit ? $funcionario->getEstado() : '') === 'MA' ? 'selected' : '' ?>>Maranhão</option>
-                                <option value="MT" <?= ($isEdit ? $funcionario->getEstado() : '') === 'MT' ? 'selected' : '' ?>>Mato Grosso</option>
-                                <option value="MS" <?= ($isEdit ? $funcionario->getEstado() : '') === 'MS' ? 'selected' : '' ?>>Mato Grosso do Sul</option>
-                                <option value="MG" <?= ($isEdit ? $funcionario->getEstado() : '') === 'MG' ? 'selected' : '' ?>>Minas Gerais</option>
-                                <option value="PA" <?= ($isEdit ? $funcionario->getEstado() : '') === 'PA' ? 'selected' : '' ?>>Pará</option>
-                                <option value="PB" <?= ($isEdit ? $funcionario->getEstado() : '') === 'PB' ? 'selected' : '' ?>>Paraíba</option>
-                                <option value="PR" <?= ($isEdit ? $funcionario->getEstado() : '') === 'PR' ? 'selected' : '' ?>>Paraná</option>
-                                <option value="PE" <?= ($isEdit ? $funcionario->getEstado() : '') === 'PE' ? 'selected' : '' ?>>Pernambuco</option>
-                                <option value="PI" <?= ($isEdit ? $funcionario->getEstado() : '') === 'PI' ? 'selected' : '' ?>>Piauí</option>
-                                <option value="RJ" <?= ($isEdit ? $funcionario->getEstado() : '') === 'RJ' ? 'selected' : '' ?>>Rio de Janeiro</option>
-                                <option value="RN" <?= ($isEdit ? $funcionario->getEstado() : '') === 'RN' ? 'selected' : '' ?>>Rio Grande do Norte</option>
-                                <option value="RS" <?= ($isEdit ? $funcionario->getEstado() : '') === 'RS' ? 'selected' : '' ?>>Rio Grande do Sul</option>
-                                <option value="RO" <?= ($isEdit ? $funcionario->getEstado() : '') === 'RO' ? 'selected' : '' ?>>Rondônia</option>
-                                <option value="RR" <?= ($isEdit ? $funcionario->getEstado() : '') === 'RR' ? 'selected' : '' ?>>Roraima</option>
-                                <option value="SC" <?= ($isEdit ? $funcionario->getEstado() : '') === 'SC' ? 'selected' : '' ?>>Santa Catarina</option>
-                                <option value="SP" <?= ($isEdit ? $funcionario->getEstado() : '') === 'SP' ? 'selected' : '' ?>>São Paulo</option>
-                                <option value="SE" <?= ($isEdit ? $funcionario->getEstado() : '') === 'SE' ? 'selected' : '' ?>>Sergipe</option>
-                                <option value="TO" <?= ($isEdit ? $funcionario->getEstado() : '') === 'TO' ? 'selected' : '' ?>>Tocantins</option>
+                            <select name="estado">
+                                <option value="">Selecione</option>
+                                <?php foreach ($estados as $sigla => $nome): ?>
+                                    <option value="<?= $sigla ?>" <?= ($isEdit ? $funcionario->getEstado() : '') === $sigla ? 'selected' : '' ?>>
+                                        <?= $nome ?>
+                                    </option>
+                                <?php endforeach; ?>
                             </select>
                         </div>
 
@@ -292,9 +307,10 @@ require_once __DIR__ . '/../includes/header.php';
 
 
 
-                        
+
                         <div class="card-contratacao">
-                            <h2>Dados de Contratação</h2>
+
+                            <h2><i class="fa-solid fa-file-signature icone-titulo"></i> Dados de Contratação</h2>
                             <div class="grupo-datas">
                                 <div class="form-group">
                                     <label>Admissão</label>
@@ -321,7 +337,7 @@ require_once __DIR__ . '/../includes/header.php';
                         <div class="card-observacao">
                             <h2>Observações</h2>
                             <textarea name="observacoes" maxlength="500"
-                            placeholder="Digite alguma observação se for necessário"><?= htmlspecialchars($isEdit ? $funcionario->getObservacoes() : '') ?></textarea>
+                                placeholder="Digite alguma observação se for necessário"><?= htmlspecialchars($isEdit ? $funcionario->getObservacoes() : '') ?></textarea>
                         </div>
                     </div>
                 </form>
@@ -335,11 +351,13 @@ require_once __DIR__ . '/../includes/header.php';
                 <?php if (!$isEdit): ?>
                     <button type="submit" form="form-dados" class="btn salvar"> <i class="bi bi-floppy"></i> Salvar</button>
                 <?php else: ?>
-                    <button type="submit" form="form-dados" class="btn alterar"><i class="bi bi-pencil-square"></i> Alterar</button>
+                    <button type="submit" form="form-dados" class="btn alterar"><i class="bi bi-pencil-square"></i>
+                        Alterar</button>
                     <a href="/ideal/public/index.php?url=funcionarios/delete&id=<?= $funcionario->getIdFuncionario() ?>"
                         class="btn excluir"
                         style="text-decoration:none; text-align:center; display:inline-block; line-height: 40px;"
-                        onclick="return confirm('Tem certeza que deseja excluir este funcionário?')"><i class="bi bi-trash"></i> Excluir</a>
+                        onclick="return confirm('Tem certeza que deseja excluir este funcionário?')"><i
+                            class="bi bi-trash"></i> Excluir</a>
                 <?php endif; ?>
 
                 <button type="reset" form="form-dados" class="btn limpar"><i class="bi bi-eraser"></i> Limpar</button>
