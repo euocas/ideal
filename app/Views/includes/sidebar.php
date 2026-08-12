fb2e45c8842d198bbd4a9ed1df3b1c8205818574
Douglas Castro <126368805+euocas@users.noreply.github.com>
2026-08-12 14:38:45 -0300

    fix: destacar apenas a página ativa no sidebar

--- app/Views/includes/sidebar.php
+++ app/Views/includes/sidebar.php
@@ -2,101 +2,105 @@
 $nomeUsuario = $_SESSION['usuario']['nome'] ?? 'Usuário';
 ?>
 
-    <!-- LOGO -->
-    <div class="logo">
-        <a href="<?= BASE_URL ?>/index.php?url=dashboard">
-            <img src="<?= BASE_URL ?>/assets/img/logo.png" alt="Logo Ideal">
+<!-- LOGO -->
+<div class="logo">
+    <a href="<?= BASE_URL ?>/index.php?url=dashboard">
+        <img src="<?= BASE_URL ?>/assets/img/logo.png" alt="Logo Ideal">
+    </a>
+</div>
+
+<?php
+$paginaAtual = $_GET['url'] ?? 'dashboard';
+?>
+
+<!-- MENU -->
+<ul class="menu">
+
+    <li>
+        <a href="index.php?url=dashboard" class="<?= $paginaAtual === 'dashboard' ? 'active' : '' ?>">
+            <i class="fa-solid fa-house"></i>
+            Home
         </a>
+    </li>
+
+    <li>
+        <a href="index.php?url=clientes" class="<?= $paginaAtual === 'clientes' ? 'active' : '' ?>">
+            <i class="fa-solid fa-users"></i>
+            Clientes
+        </a>
+    </li>
+
+    <li>
+        <a href="index.php?url=obras" class="<?= $paginaAtual === 'obras' ? 'active' : '' ?>">
+            <i class="fa-solid fa-building"></i>
+            Obras
+        </a>
+    </li>
+
+    <li>
+        <a href="index.php?url=veiculos" class="<?= $paginaAtual === 'veiculos' ? 'active' : '' ?>">
+            <i class="fa-solid fa-car"></i>
+            Veículos
+        </a>
+    </li>
+
+    <li>
+        <a href="index.php?url=funcionarios" class="<?= $paginaAtual === 'funcionarios' ? 'active' : '' ?>">
+            <i class="fa-solid fa-user-tie"></i>
+            Funcionários
+        </a>
+    </li>
+
+    <li>
+        <a href="index.php?url=financeiros" class="<?= $paginaAtual === 'financeiros' ? 'active' : '' ?>">
+            <i class="fa-solid fa-wallet"></i>
+            Financeiro
+        </a>
+    </li>
+
+    <li>
+        <a href="index.php?url=relatorios" class="<?= $paginaAtual === 'relatorios' ? 'active' : '' ?>">
+            <i class="fa-solid fa-chart-line"></i>
+            Relatórios
+        </a>
+    </li>
+
+    <!-- CONFIGURAÇÕES -->
+    <div class="menu-title">
+        CONFIGURAÇÕES
     </div>
 
-    <!-- MENU -->
-    <ul class="menu">
+    <li>
+        <a href="index.php?url=credenciais" class="<?= $paginaAtual === 'credenciais' ? 'active' : '' ?>">
+            <i class="fa-solid fa-key"></i>
+            Trocar Senha
+        </a>
+    </li>
 
-        <li>
-            <a href="index.php?url=dashboard" class="active">
-                <i class="fa-solid fa-house"></i>
-                Home
-            </a>
-        </li>
+    <li>
+        <a href="index.php?url=logout">
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            Sair
+        </a>
+    </li>
 
-        <li>
-            <a href="index.php?url=clientes">
-                <i class="fa-solid fa-users"></i>
-                Clientes
-            </a>
-        </li>
+</ul>
 
-        <li>
-            <a href="index.php?url=obras">
-                <i class="fa-solid fa-building"></i>
-                Obras
-            </a>
-        </li>
+<!-- USUÁRIO -->
+<div class="user">
 
-        <li>
-            <a href="index.php?url=veiculos">
-                <i class="fa-solid fa-car"></i>
-                Veículos
-            </a>
-        </li>
+    <div class="avatar">
+        <?= strtoupper(substr($nomeUsuario, 0, 1)); ?>
+    </div>
 
-        <li>
-            <a href="index.php?url=funcionarios">
-                <i class="fa-solid fa-user-tie"></i>
-                Funcionários
-            </a>
-        </li>
+    <div class="user-info">
 
-        <li>
-            <a href="index.php?url=financeiros">
-                <i class="fa-solid fa-wallet"></i>
-                Financeiro
-            </a>
-        </li>
+        <h3><?= $nomeUsuario; ?></h3>
 
-        <li>
-            <a href="index.php?url=relatorios">
-                <i class="fa-solid fa-chart-line"></i>
-                Relatórios
-            </a>
-        </li>
-
-        <!-- CONFIGURAÇÕES -->
-        <div class="menu-title">
-            CONFIGURAÇÕES
-        </div>
-
-        <li>
-            <a href="index.php?url=credenciais">
-                <i class="fa-solid fa-key"></i>
-                Trocar Senha
-            </a>
-        </li>
-
-        <li>
-            <a href="index.php?url=logout">
-                <i class="fa-solid fa-arrow-right-from-bracket"></i>
-                Sair
-            </a>
-        </li>
-
-    </ul>
-
-    <!-- USUÁRIO -->
-    <div class="user">
-
-        <div class="avatar">
-            <?= strtoupper(substr($nomeUsuario, 0, 1)); ?>
-        </div>
-
-        <div class="user-info">
-
-            <h3><?= $nomeUsuario; ?></h3>
-
-            <span>
-                Usuário do sistema
-            </span>
-
-        </div>
+        <span>
+            Usuário do sistema
+        </span>
 
     </div>
+
+</div>
